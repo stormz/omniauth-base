b7a8b9a1ca5df5c5554a21b1ded5e66a3d008e3b
François de Metz <francois@2metz.fr>
2015-06-10 11:05:03 +0200

    Fix class name.

--- lib/omniauth/base.rb
+++ lib/omniauth/base.rb
@@ -2,7 +2,7 @@ require 'omniauth-oauth2'
 
 module OmniAuth
   module Strategies
-    class SomeSite < OmniAuth::Strategies::OAuth2
+    class Base < OmniAuth::Strategies::OAuth2
       option :name, "base"
 
       option :client_options, {:site => "https://api.getbase.com"}
