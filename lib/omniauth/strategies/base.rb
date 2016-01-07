ca30ca0e72028228592ab9fdad44615d3876769f
Florian Ebeling <florian.ebeling@gmail.com>
2016-01-07 19:58:50 +0100

    Use provider compatible #callback_url impl
    The implementation of #callback_url in omniauth-oauth2 strategy
    changed in a way that is correct according to oauth spec, but breaks
    many oauth providers.
    
    This change adapts this strategy to the old behavior. That way
    omniauth-oauth2 1.4 can be used. That is better, as version 1.3.1 seems
    to be incompatible with Rails 5.

--- lib/omniauth/strategies/base.rb
+++ lib/omniauth/strategies/base.rb
@@ -29,6 +29,10 @@ module OmniAuth
       def raw_info
         @raw_info ||= access_token.get('/v2/users/self').parsed
       end
+
+      def callback_url
+        full_host + script_name + callback_path
+      end
     end
   end
 end
